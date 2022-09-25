790d90e89f55bb3c085f8c61d925ab05c43426c7
wavecat#7207 <81644771+devanwashere@users.noreply.github.com>
2022-09-25 16:36:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// https://rawgit.com/ckrack/scrollsnap-polyfill/develop/dist/scrollsnap-polyfill.bundled.js
+// i dont need pollyfill anymore https://rawgit.com/ckrack/scrollsnap-polyfill/develop/dist/scrollsnap-polyfill.bundled.js
+
+const videos = document.querySelectorAll("video"); // Select ALL the Videos
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      entry.target.pause(); // Pause the TARGET video
+    } else {
+      entry.target.play(); // Play the TARGET video
+    }
+  });
+}, {});
+for (const video of videos) observer.observe(video); // Observe EACH video
+const onVisibilityChange = () => {
+  if (document.hidden) {
+    for (const video of videos) video.pause(); // Pause EACH video
+  } else {
+    for (const video of videos) video.play(); // Play EACH video
+  }
+};
+document.addEventListener("visibilitychange", onVisibilityChange);
